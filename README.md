1684750068ed99e2a497703f0c1867585c544e1d
kim <kim@basking.io>
2019-03-22 23:15:02 +0100

    add basic of slack bot

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# BaskingLunchBot
-What To Eat Today? -> The Biggest Question
+
+```bash
+export BOT_ID="U-------F"           
+export BOT_TOKEN="xoxb--------"      
+export VERIFICATION_TOKEN="G-------2" 
+export CHANNEL_ID="C-------"      
+```
+
+env is in one password!
+
+
+```
+$ dep ensure
+```
---
+++ slack.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/nlopes/slack"
+)
+
+const (
+	// action is used for slack attament action.
+	actionSelect = "select"
+	actionStart  = "start"
+	actionCancel = "cancel"
+)
+
+type SlackListener struct {
+	client    *slack.Client
+	botID     string
+	channelID string
+}
+
+// LstenAndResponse listens slack events and response
+// particular messages. It replies by slack message button.
+func (s *SlackListener) ListenAndResponse() {
+	rtm := s.client.NewRTM()
+
+	// Start listening slack events
+	go rtm.ManageConnection()
+
+	// Handle slack events
+	for msg := range rtm.IncomingEvents {
+		switch ev := msg.Data.(type) {
+		case *slack.MessageEvent:
+			if err := s.handleMessageEvent(ev); err != nil {
+				log.Printf("[ERROR] Failed to handle message: %s", err)
+			}
+		}
+	}
+}
+
+// handleMesageEvent handles message events.
+func (s *SlackListener) handleMessageEvent(ev *slack.MessageEvent) error {
+
+	// Only response in specific channel. Ignore else.
+	if ev.Channel != s.channelID {
+		log.Printf("%s %s", ev.Channel, ev.Msg.Text)
+		return nil
+	}
+
+	// Only response mention to bot. Ignore else.
+	if !strings.HasPrefix(ev.Msg.Text, fmt.Sprintf("<@%s> ", s.botID)) {
+		return nil
+	}
+
+	// Parse message
+	m := strings.Split(strings.TrimSpace(ev.Msg.Text), " ")[1:]
+	// if len(m) == 0 || m[0] != "hey" {
+	if len(m) == 0 {
+		return fmt.Errorf("invalid message")
+	}
+
+	attachment := slack.Attachment{
+		Text:       "Today's Menu? :spoon:",
+		Color:      "#f9a41b",
+		CallbackID: "menu",
+		Actions: []slack.AttachmentAction{
+			{
+				Name: actionSelect,
+				Type: "select",
+				Options: []slack.AttachmentActionOption{
+					{
+						Text:  "Asian",
+						Value: "Asian",
+					},
+					{
+						Text:  "European",
+						Value: "European",
+					},
+					{
+						Text:  "Simple",
+						Value: "Simple",
+					},
+					{
+						Text:  "Junk Food",
+						Value: "Junk",
+					},
+					{
+						Text:  "Nearest",
+						Value: "Nearest",
+					},
+				},
+			},
+
+			{
+				Name:  actionCancel,
+				Text:  "Cancel",
+				Type:  "button",
+				Style: "danger",
+			},
+		},
+	}
+
+	params := slack.PostMessageParameters{
+		Attachments: []slack.Attachment{
+			attachment,
+		},
+	}
+
+	if _, _, err := s.client.PostMessage(ev.Channel, "", params); err != nil {
+		return fmt.Errorf("failed to post message: %s", err)
+	}
+
+	return nil
+}
